a89b46f9fe4f013f5be005e7aa2b64932bdd4301
Tunde Oladipupo <simplytunde@gmail.com>
2021-01-12 07:07:35 -0600

    updated ci values

--- .gitlab-ci/jobs/rke2/dependencies/terraform/env/ci-airgap/ci.tf
+++ .gitlab-ci/jobs/rke2/dependencies/terraform/env/ci-airgap/ci.tf
@@ -35,22 +35,14 @@ data "terraform_remote_state" "utility" {
   workspace = var.env
 }
 
-data "template_file" "pre_userdata" {
-  template = file("${path.module}/pre_userdata.tpl")
-  vars = {
-     registry_username = data.terraform_remote_state.utility.outputs.utility_username
-     registry_password = data.terraform_remote_state.utility.outputs.utility_password
-     cidr_block   = data.aws_vpc.selected.cidr_block
-  }
-}
-
 module "ci" {
   source = "../../main"
 
-  env               = var.env
-  vpc_id            = data.terraform_remote_state.networking.outputs.vpc_id
-  deploy_subnets    = data.terraform_remote_state.networking.outputs.private_subnets
-  public_subnets    = data.terraform_remote_state.networking.outputs.public_subnets
-  pre_userdata      = data.template_file.pre_userdata.rendered
-  airgap            = var.airgap
+  env                 = var.env
+  vpc_id              = data.terraform_remote_state.networking.outputs.vpc_id
+  deploy_subnets      = data.terraform_remote_state.networking.outputs.private_subnets
+  public_subnets      = data.terraform_remote_state.networking.outputs.public_subnets
+  airgap              = false
+  registry_username   = data.terraform_remote_state.utility.outputs.utility_username
+  registry_password   = data.terraform_remote_state.utility.outputs.utility_password
 }
--- .gitlab-ci/jobs/rke2/dependencies/terraform/env/ci/ci.tf
+++ .gitlab-ci/jobs/rke2/dependencies/terraform/env/ci/ci.tf
@@ -19,25 +19,12 @@ data "terraform_remote_state" "networking" {
   workspace = var.env
 }
 
-data "terraform_remote_state" "utility" {
-  backend = "s3"
-  config = {
-    bucket               = "umbrella-tf-states"
-    key                  = "terraform.tfstate"
-    region               = "us-gov-west-1"
-    workspace_key_prefix = "utility"
-  }
-  workspace = var.env
-}
-
 module "ci" {
   source = "../../main"
 
-  env                = var.env
+  env                 = var.env
   vpc_id              = data.terraform_remote_state.networking.outputs.vpc_id
   deploy_subnets      = data.terraform_remote_state.networking.outputs.private_subnets
   public_subnets      = data.terraform_remote_state.networking.outputs.public_subnets
-  airgap              = true
-  registry_username   = data.terraform_remote_state.utility.outputs.utility_username
-  registry_password   = data.terraform_remote_state.utility.outputs.utility_password
+  airgap              = false
 }
