75aa4f984366e9133a49aa28ba8958ce2c26df60
Zachariah Dzielinski <dzielinski_zachariah@bah.com>
2021-02-02 12:17:37 -0700

    ci test

--- scripts/deploy/02_wait_for_helmrealeases.sh
+++ scripts/deploy/02_wait_for_helmrealeases.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 ## This is an array to instantiate the order of wait conditions
 ORDERED_HELMRELEASES="gatekeeper istio-operator istio monitoring eck-operator ek fluent-bit twistlock cluster-auditor authservice argocd gitlab haproxy-sso"
